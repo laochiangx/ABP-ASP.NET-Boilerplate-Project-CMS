b969236593eb527829f9b4bcbac70dd59a8ca1d4
Jimmey-Jiang <2181130@qq.com>
2017-11-15 12:29:07 +0800

    swagger

--- src/ABPCMS.WebApi/Api/ABPCMSWebApiModule.cs
+++ src/ABPCMS.WebApi/Api/ABPCMSWebApiModule.cs
@@ -4,6 +4,9 @@ using Abp.Application.Services;
 using Abp.Configuration.Startup;
 using Abp.Modules;
 using Abp.WebApi;
+using System.Linq;
+using System;
+using System.IO;
 
 namespace ABPCMS.Api
 {
@@ -16,9 +19,33 @@ namespace ABPCMS.Api
 
             Configuration.Modules.AbpWebApi().DynamicApiControllerBuilder
                 .ForAll<IApplicationService>(typeof(ABPCMSApplicationModule).Assembly, "app")
+                .WithConventionalVerbs()
                 .Build();
 
             Configuration.Modules.AbpWebApi().HttpConfiguration.Filters.Add(new HostAuthenticationFilter("Bearer"));
+
+            //ConfigureSwaggerUi();
         }
+
+        //private void ConfigureSwaggerUi()
+        //{
+        //    Configuration.Modules.AbpWebApi().HttpConfiguration
+        //        .EnableSwagger(c =>
+        //        {
+        //            c.SingleApiVersion("v1", "API文档");
+        //            c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+
+        //            //将application层中的注释添加到SwaggerUI中
+        //            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+        //            var commentsFileName = "bin//ABP.CMS.Application.xml";
+        //            var commentsFile = Path.Combine(baseDirectory, commentsFileName);
+        //            //将注释的XML文档添加到SwaggerUI中
+        //            c.IncludeXmlComments(commentsFile);
+
+        //        })
+        //      .EnableSwaggerUi(c => c.InjectJavaScript(Assembly.GetExecutingAssembly(), " ABPCMS.SwaggerUI.Scripts.wagger.js"));
+
+        //}
     }
 }
